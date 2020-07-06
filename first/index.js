1d4529f8c02e84c4b26a5b2196e3c3b33594e7f6
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-07-06 10:00:37 +0300

    First tests
    First tests

---
+++ first/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// JSX
+ReactDOM.render(<div>
+    <ul>
+        <li>Bananas</li>
+        <li>Apples</li>
+        <li>Kiwis</li>
+    </ul>
+    </div>, document.getElementById("root"));
