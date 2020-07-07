ca6a73371501ea8b86b0a79ce502b23e0089f8b1
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-07-07 12:45:53 +0300

    A new attempt at npm run deployment

--- quiz-app/quiz-app/build/service-worker.js
+++ quiz-app/quiz-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6e32b6a58470f3ad8aa8c82bfd4d222.js"
+  "/react/precache-manifest.7e5f1bfc33a3fce515cad8f2fa234b23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
