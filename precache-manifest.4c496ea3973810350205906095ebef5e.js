9209ecaae0888410e926b14dcaf835fedcd4fcd2
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-07-08 08:22:54 +0300

    Updates

---
+++ precache-manifest.4c496ea3973810350205906095ebef5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482d8147baeda80b3c4f7de795047224",
+    "url": "/react/recipe-app/index.html"
+  },
+  {
+    "revision": "7f7c3e450ebd76e101d7",
+    "url": "/react/recipe-app/static/css/main.38b31f8b.chunk.css"
+  },
+  {
+    "revision": "a6d1ebf6515c91a53dde",
+    "url": "/react/recipe-app/static/js/2.60e0b9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/recipe-app/static/js/2.60e0b9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7c3e450ebd76e101d7",
+    "url": "/react/recipe-app/static/js/main.a09cadd4.chunk.js"
+  },
+  {
+    "revision": "1b0e76301a6089412ef7",
+    "url": "/react/recipe-app/static/js/runtime-main.636d21d7.js"
+  }
+]);
