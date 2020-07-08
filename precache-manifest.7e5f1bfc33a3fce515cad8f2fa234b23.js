29a2f8e2d754bf0fb519472f6f5500a944344546
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-07-08 09:49:00 +0300

    Updates

---
+++ precache-manifest.7e5f1bfc33a3fce515cad8f2fa234b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5dd45bbb4a61ce9895f7e2795b6a1f0",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "1a694a591b4c8dc4423f",
+    "url": "/react/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "97b8cb1fdfd0a2509bb3",
+    "url": "/react/static/js/2.5abaa134.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.5abaa134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a694a591b4c8dc4423f",
+    "url": "/react/static/js/main.d68b7206.chunk.js"
+  },
+  {
+    "revision": "d449a9d507391a027780",
+    "url": "/react/static/js/runtime-main.707d94b6.js"
+  }
+]);
