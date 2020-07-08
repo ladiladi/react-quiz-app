34cf19e08e57a480d260767430ae5a8061333064
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-07-08 09:51:22 +0300

    Made for a single app reposition because of issues with gh-pages deployment

---
+++ quiz-app/quiz-app/build/precache-manifest.14500c065b79828a083b7e843d33fb4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15abb2dfc019209f616e3e30ad7c9ae6",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "1a694a591b4c8dc4423f",
+    "url": "/react-quiz-app/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "97b8cb1fdfd0a2509bb3",
+    "url": "/react-quiz-app/static/js/2.5abaa134.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.5abaa134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a694a591b4c8dc4423f",
+    "url": "/react-quiz-app/static/js/main.d68b7206.chunk.js"
+  },
+  {
+    "revision": "f6a3892533c386f8c908",
+    "url": "/react-quiz-app/static/js/runtime-main.c846ca35.js"
+  }
+]);
